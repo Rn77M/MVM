e7dbd007bc2f94ec38a3cc72952873b5a14fcc3f
Rn77M <133021118+Rn77M@users.noreply.github.com>
2024-01-23 13:51:44 -0300

    Update Croche.js

--- components/pages/Croche.js
+++ components/pages/Croche.js
@@ -1,10 +1,11 @@
-import styles from './Croche.module.css'
-import Terezinha from '../../img/WhatsApp Image 2024-01-21 at 15.43.57.jpeg'
-import Benedito from '../../img/WhatsApp Image 2024-01-21 at 15.43.57 (1).jpeg'
-import VestidoVermelho from '../../img/WhatsApp Image 2024-01-21 at 15.43.48 (2).jpeg'
-import VestidoVermelhoDois from '../../img/WhatsApp Image 2024-01-21 at 15.43.48.jpeg'
-import SaidaPraia from '../../img/720767f4-1bd5-481f-85d0-e47be4e0d9db.jpg'
+import styles from './Croche.module.css';
+import Terezinha from '../../img/WhatsApp Image 2024-01-21 at 15.43.57.jpeg';
+import Benedito from '../../img/WhatsApp Image 2024-01-21 at 15.43.57 (1).jpeg';
+import VestidoVermelho from '../../img/WhatsApp Image 2024-01-21 at 15.43.48 (2).jpeg';
+import VestidoVermelhoDois from '../../img/WhatsApp Image 2024-01-21 at 15.43.48.jpeg';
+import SaidaPraia from '../../img/720767f4-1bd5-481f-85d0-e47be4e0d9db.jpg';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import LinkButton from '../layout/LinkButton';
 
 function Croche(){
     return (
@@ -16,10 +17,14 @@ function Croche(){
             <Link to="/contact">
               <img src={Terezinha} className={styles.terezinha} title="Gostou da Arte? Clique Aqui!"/>
             </Link>
+            
+            <LinkButton className={styles.link} to="/contact" text="Compre Agora"/>
             <h2>São Benedito</h2>
             <Link to="/contact">
               <img src={Benedito} className={styles.benedito} title="Gostou da Arte? Clique Aqui!"/>
             </Link>
+            
+            <LinkButton to="/contact" text="Compre Agora"/>
           </section>
           <section className={styles.croche_container}>
             <h1>Vestidos de Crochê</h1>
@@ -27,14 +32,20 @@ function Croche(){
             <Link to="/contact">
               <img src={VestidoVermelho} className={styles.vestido} title="Gostou da Arte? Clique Aqui!"/>
             </Link>
+            
+            <LinkButton to="/contact" text="Compre Agora"/>
             <h2>Saida de Praia</h2>
             <Link to="/contact">
               <img src={SaidaPraia} className={styles.saida_praia} title="Gostou da Arte? Clique Aqui!"/>
             </Link>
+            
+            <LinkButton to="/contact" text="Compre Agora"/>
             <h2>Saia - Vestido</h2>
             <Link to="/contact">
               <img src={VestidoVermelhoDois} className={styles.vestido} title="Gostou da Arte? Clique Aqui!"/>
             </Link>
+            
+            <LinkButton to="/contact" text="Compre Agora"/>
           </section>
         </>
     )
@@ -44,6 +55,7 @@ function Croche(){
 
 
 export default Croche;
+
 
 
 
